0fb93e86cdf587d31d898a1676a02ec0499cfffb
lucasalvess96 <lucasalves.57@hotmail.com>
2024-05-30 15:28:54 -0300

    fix: ajustando recurso download de pdf

--- src/main/java/com/arquivos/uploadsdownloads/pdf/downloads/PdfController.java
+++ src/main/java/com/arquivos/uploadsdownloads/pdf/downloads/PdfController.java
@@ -3,6 +3,7 @@ package com.arquivos.uploadsdownloads.pdf.downloads;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -17,12 +18,17 @@ public class PdfController {
         this.pdfService = pdfService;
     }
 
-    @GetMapping("/download")
-    public ResponseEntity<String> downloadPdf(@RequestParam String url, @RequestParam String destination) {
+    @PostMapping("/download")
+    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile file) {
         try {
-            String downloadedFileName = pdfService.generatedPDF(url, destination);
-            String message = String.format("Arquivo '%s' baixado com sucesso.", downloadedFileName);
-            return ResponseEntity.ok(message);
+            String downloadedFileName = pdfService.generatedPDF(file);
+            if (downloadedFileName != null) {
+                String message = String.format("Arquivo '%s' baixado com sucesso.", downloadedFileName);
+                return ResponseEntity.ok(message);
+            } else {
+                String errorMessage = "Erro ao processar o arquivo: o conteúdo não é um PDF.";
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+            }
         } catch (IOException e) {
             String errorMessage = "Erro ao baixar o arquivo: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
--- src/main/java/com/arquivos/uploadsdownloads/pdf/downloads/PdfService.java
+++ src/main/java/com/arquivos/uploadsdownloads/pdf/downloads/PdfService.java
@@ -1,39 +1,40 @@
 package com.arquivos.uploadsdownloads.pdf.downloads;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
 public class PdfService {
 
-    final Logger logger = Logger.getLogger(PdfService.class.getName());
+    private static final Logger logger = Logger.getLogger(PdfService.class.getName());
 
-    public String generatedPDF(String url, String destination) throws IOException {
-        URL pdfUrl = new URL(url);
-        URLConnection urlConnection = pdfUrl.openConnection();
-        if (!urlConnection.getContentType().equalsIgnoreCase("application/pdf")) {
-            logger.log(Level.WARNING, "O conteúdo não é um PDF: {0}", url);
+    private static final String DOWNLOAD_DIR = "C:\\Users\\Micro\\Downloads";
+
+    public String generatedPDF(MultipartFile file) throws IOException {
+        if (!Objects.requireNonNull(file.getContentType()).equalsIgnoreCase("application/pdf")) {
+            logger.log(Level.WARNING, "O conteúdo não é um PDF: {0}", file.getOriginalFilename());
             return null;
         }
-        try (InputStream inputStream = pdfUrl.openStream();
-             FileOutputStream outputStream = new FileOutputStream(destination)) {
+        String destinationPath = Paths.get(DOWNLOAD_DIR, file.getOriginalFilename()).toString();
+        File destFile = new File(destinationPath);
+        try (InputStream inputStream = file.getInputStream();
+             FileOutputStream outputStream = new FileOutputStream(destFile)) {
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) > 0) {
                 outputStream.write(buffer, 0, bytesRead);
             }
-            logger.log(Level.INFO, "Download concluído com sucesso: {0}", destination);
-            return destination;
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Erro durante a leitura/escrita do arquivo: {0}", e.getMessage());
-            return null;
+            logger.log(Level.INFO, "Download concluído com sucesso: {0}", destFile.getAbsolutePath());
+            return destFile.getAbsolutePath();
         }
     }
 }
